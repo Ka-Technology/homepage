5509ed840bf2e5ea15cb9965b2cdd3791e9ebe2e
mrkangs <gykang00@gmail.com>
2024-06-29 18:15:54 -0400

    add basic schemas for the backend

--- backend/schemaTypes/index.js
+++ backend/schemaTypes/index.js
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import people from './people';
+import contact from './contact';
+import about from './about';
+import project from './project';
+import project_overview from './project_overview';
+
+
+export const schemaTypes = [about, contact, people, project, project_overview]
