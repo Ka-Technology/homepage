54d5874249059c6f12a4f1d1b69ef826d87b628d
Andrew Dang <danga@oregonstate.edu>
2024-07-19 16:28:41 -0700

    fixing colors

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -22,15 +22,9 @@ const Navbar = () => {
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+      <li><a>Services</a></li>
+      <li><a>Products</a></li>
+      <li><a>About Us</a></li>
       </ul>
     </div>
     <a className="btn btn-ghost text-xl">KaTech</a>
