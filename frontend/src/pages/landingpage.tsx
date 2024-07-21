412cef4dbc4d58754ff4ce7abf1b5ddeb9af3437
Andrew <129807999+TheAndrewDang@users.noreply.github.com>
2024-07-21 02:06:59 -0700

    Added footer

--- frontend/src/pages/landingpage.tsx
+++ frontend/src/pages/landingpage.tsx
@@ -181,6 +181,11 @@ const LandingPage = () => {
           </div>
         </div>
       </div>
+        <footer className="footer footer-center bg-base-300 text-base-content p-4">
+        <aside>
+          <p>Copyright © {new Date().getFullYear()} - All right reserved by Ka Technologies</p>
+        </aside>
+      </footer>
     </div>
   );
 }
