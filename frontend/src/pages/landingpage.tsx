f976ea274b6d5d44c603b74ba94baf2c532e1498
Andrew <129807999+TheAndrewDang@users.noreply.github.com>
2024-08-03 19:16:48 -0700

    New Services Panel

--- frontend/src/pages/landingpage.tsx
+++ frontend/src/pages/landingpage.tsx
@@ -1,10 +1,81 @@
+"use client";
 import React, { useEffect, useRef, useState } from "react";
-import { CardDemo } from "../components/ui/animatedcard"
-import {client, urlFor} from '../client';
+import { client, urlFor } from '../client';
 import emailjs from "@emailjs/browser";
+import { Carousel, Card } from "../components/ui/apple-cards-carousel";
+
+// DummyContent Component
+const DummyContent = () => {
+  return (
+    <>
+      {[...new Array(3).fill(1)].map((_, index) => (
+        <div
+          key={"dummy-content" + index}
+          className="bg-[#F5F5F7] p-8 md:p-14 rounded-3xl mb-4"
+        >
+          <p className="text-neutral-600 text-base md:text-2xl font-sans max-w-3xl mx-auto">
+            <span className="font-bold text-neutral-700">
+              Test
+            </span>{" "}
+            Test
+          </p>
+          <img
+            src="https://assets.aceternity.com/macbook.png"
+            alt="Macbook mockup from Aceternity UI"
+            height="500"
+            width="500"
+            className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
+          />
+        </div>
+      ))}
+    </>
+  );
+};
+
+// Data Array for the Carousel
+const data = [
+  {
+    category: "Artificial Intelligence",
+    title: "You can do more with AI.",
+    src: "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?q=80&w=3556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content: <DummyContent />,
+  },
+  {
+    category: "Productivity",
+    title: "Enhance your productivity.",
+    src: "https://images.unsplash.com/photo-1531554694128-c4c6665f59c2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content: <DummyContent />,
+  },
+  {
+    category: "Artificial Intelligence",
+    title: "You can do more with AI.",
+    src: "https://images.unsplash.com/photo-1713869791518-a770879e60dc?q=80&w=2333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content: <DummyContent />,
+  },
+  {
+    category: "Artificial Intelligence",
+    title: "You can do more with AI.",
+    src: "https://images.unsplash.com/photo-1599202860130-f600f4948364?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    content: <DummyContent />,
+  },
+];
+
+export function AppleCardsCarouselDemo() {
+  const cards = data.map((card, index) => (
+    <Card key={card.src} card={card} index={index} />
+  ));
+
+  return (
+    <div className="w-full h-full py-20">
+      <h2 className="text-center mx-auto text-xl md:text-5xl font-bold">
+        Services
+      </h2>
+      <Carousel items={cards} />
+    </div>
+  );
+}
 
 const LandingPage = () => {
-
   // Set variables to hold the query
   const [people, setPeople] = useState([]);
   const [services, setServices] = useState([]);
@@ -12,26 +83,25 @@ const LandingPage = () => {
   const [loading, setLoading] = useState(true);
 
   // Set variable for data input from contact
-  const [formData, setFormData] = useState({name: '', email: '', message: ''});
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
   const [isFormSubmitted, setisFormSubmitted] = useState(false);
   const [loadingSubmit, setloadingSubmit] = useState(false);
 
   // Structure the formData
-  const {name, email, message} = formData;
+  const { name, email, message } = formData;
 
   // Set reference for animation
   const observerRef = useRef<IntersectionObserver | null>(null);
 
   // client connection
   useEffect(() => {
-    const fetchData = async() => {
+    const fetchData = async () => {
       try {
         // fetch service data
         const queryService = '*[_type == "service"] | order(priority asc)';
         client.fetch(queryService).then((serviceData) => {
           setServices(serviceData);
-          });
-
+        });
 
         // fetch service overview data
         const queryServiceOverview = '*[_type == "service_overview"] | order(priority asc)';
@@ -47,7 +117,6 @@ const LandingPage = () => {
 
         // loading complete
         setLoading(false);
-
       } catch (error) {
         console.error('Error fetching data', error);
         setLoading(false);
@@ -57,17 +126,16 @@ const LandingPage = () => {
     fetchData();
   }, []);
 
-
   // This is for the fancy animations you see on the Teams section
   useEffect(() => {
     const options = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.1
+      threshold: 0.1,
     };
 
     const callback = (entries: IntersectionObserverEntry[]) => {
-      entries.forEach(entry => {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add('animate');
         }
@@ -76,7 +144,7 @@ const LandingPage = () => {
 
     observerRef.current = new IntersectionObserver(callback, options);
     const teamBlocks = document.querySelectorAll('.teamblock');
-    teamBlocks.forEach(block => {
+    teamBlocks.forEach((block) => {
       observerRef.current?.observe(block);
     });
 
@@ -87,16 +155,16 @@ const LandingPage = () => {
 
   // handle key input for message sent
   const handleChangeInput = (e: any) => {
-    const {name, value} = e.target;
-    
-    setFormData({ ...formData, [name]: value});
-  }
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  };
 
   // Dealing handle submit for contact
   const handleSubmit = () => {
     if (!name.trim() || !email.trim() || !message.trim()) {
-        alert('Warning: All fields must be filled out.');
-        return;
+      alert('Warning: All fields must be filled out.');
+      return;
     }
 
     // Email validation regex
@@ -104,90 +172,58 @@ const LandingPage = () => {
 
     // Check if email is valid
     if (!emailRegex.test(email)) {
-        alert('Warning: Please enter a valid email address.');
-        return;
+      alert('Warning: Please enter a valid email address.');
+      return;
     }
 
     setloadingSubmit(true);
 
     const contact = {
-        _type: 'contact',
-        name: name,
-        email: email,
-        message: message,
-    }
+      _type: 'contact',
+      name: name,
+      email: email,
+      message: message,
+    };
 
-    client.create(contact)
-        .then(() => {
-            setloadingSubmit(false);
-            setisFormSubmitted(true);
-        })
-        .catch(error => {
-            setloadingSubmit(false);
-            console.error('Error submitting form:', error);
-        });
-    
+    client
+      .create(contact)
+      .then(() => {
+        setloadingSubmit(false);
+        setisFormSubmitted(true);
+      })
+      .catch((error) => {
+        setloadingSubmit(false);
+        console.error('Error submitting form:', error);
+      });
+
     emailjs.init(import.meta.env.VITE_EMAIL_USER_ID);
     emailjs
-        .send(
-          import.meta.env.VITE_EMAIL_SERVICE_ID,
-          import.meta.env.VITE_EMAIL_TEMPLATE_ID,
-          {
-            name: name,
-            email: email,
-            message: message,
-          }
-        )
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    });  
-};
-
-
-  
-
-  // const handleScroll = () => {
-  //   const sections = document.querySelectorAll('.scroll-section') as NodeListOf<HTMLElement>;
-  //   const stickyImage = document.getElementById('sticky-image') as HTMLImageElement;
-  //   const scrollY = window.scrollY;
-  //   const threshold = window.innerHeight / 2;
-
-  //   sections.forEach((section, index) => {
-  //     const sectionTop = section.offsetTop;
-  //     const sectionHeight = section.offsetHeight;
-
-  //     if (scrollY >= sectionTop - threshold && scrollY < sectionTop + sectionHeight - threshold) {
-  //       if (stickyImage) {
-  //         if (stickyImage.dataset.src !== serviceImages[index]) {
-  //           stickyImage.style.opacity = '0';
-  //           setTimeout(() => {
-  //             stickyImage.src = serviceImages[index];
-  //             stickyImage.dataset.src = serviceImages[index];
-  //             stickyImage.style.opacity = '1'; 
-  //           }, 150);
-  //         }
-  //       }
-  //     }
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener('scroll', handleScroll);
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, []);
+      .send(
+        import.meta.env.VITE_EMAIL_SERVICE_ID,
+        import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+        {
+          name: name,
+          email: email,
+          message: message,
+        }
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   if (loading) {
-    return <div className="flex w-52 flex-col gap-4">
-            <div className="skeleton h-32 w-full"></div>
-            <div className="skeleton h-4 w-28"></div>
-            <div className="skeleton h-4 w-full"></div>
-            <div className="skeleton h-4 w-full"></div>
-          </div>;
+    return (
+      <div className="flex w-52 flex-col gap-4">
+        <div className="skeleton h-32 w-full"></div>
+        <div className="skeleton h-4 w-28"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+      </div>
+    );
   }
 
   return (
@@ -209,60 +245,29 @@ const LandingPage = () => {
           </div>
         </div>
       </div>
-      <div id="services" className="hero min-h-screen">
-        <div className="text-center hero-content">
-          <div className="max-w-5xl mx-auto px-4">
-            <h1 className="mb-5 text-5xl font-bold">
-              <span className="font-bold tracking-tight text-left animate-gradient bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent"> 
-                Services
-              </span>
-            </h1>
-            <p className="mb-5">
-              Offering the future of your business today.
-            </p>
-
-            <div className="flex">
-              <div className="w-1/2 h-screen sticky top-0 flex items-center justify-center">
-                  <CardDemo />
-              </div>
-              <div className="w-1/2 flex flex-col justify-center">
-                {services.map((service: {name: any, description: any}, index) => (
-                  <div key={index} className="scroll-section min-h-screen p-8 flex items-center">
-                    <div>
-                      <h2 className="text-4xl font-bold mb-4">{service.name}</h2>
-                      <p className="text-lg ">{service.description}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
+      <div id="services" className="hero min-h-screen max-w-screen">
+        <AppleCardsCarouselDemo />
       </div>
-            
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {serviceOverviews.map((serviceOverview: {name: any, description: any, slug: any}, index) => (
+      {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {serviceOverviews.map((serviceOverview: { name: any, description: any, slug: any }, index) => (
           <div key={index} className="card bg-base-100 shadow-xl">
-            {/* add the simple icon instead of the image here */}
             <figure className="px-15 pt-15">
               <img
                 src="https://walker-web.imgix.net/cms/Gradient_builder_2.jpg?auto=format,compress&w=1920&h=1200&fit=crop"
                 alt="AI Database"
-                className="rounded-xl" />
+                className="rounded-xl"
+              />
             </figure>
-            <div  className="card-body items-center text-center">
+            <div className="card-body items-center text-center">
               <h2 className="card-title">{serviceOverview.name}</h2>
-                <p>{serviceOverview.description}</p>
-                <div className="card-actions">
-                  {/* Add the slug link later once we have template pages for the rate and such */}
-                  <button className="btn bg-katech-red border-katech-red text-white hover:bg-red-600 hover:border-red-600">See Rates</button> 
-                </div>
-
+              <p>{serviceOverview.description}</p>
+              <div className="card-actions">
+                <button className="btn bg-katech-red border-katech-red text-white hover:bg-red-600 hover:border-red-600">See Rates</button>
+              </div>
             </div>
-        </div>
+          </div>
         ))}
-      </div>
-
+      </div> */}
       <div id="team" className="hero min-h-screen">
         <div className="text-center hero-content">
           <div className="max-w-max mx-auto px-4">
@@ -297,12 +302,11 @@ const LandingPage = () => {
           </div>
         </div>
       </div>
-
       <div id="contact" className="hero min-h-screen">
         <div className="text-center hero-content">
           <div className="max-w-max mx-auto px-4">
             <h1 className="mb-5 text-5xl font-bold">
-              <span className="font-bold tracking-tight text-left animate-gradient bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent"> 
+              <span className="font-bold tracking-tight text-left animate-gradient bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent">
                 Contact Us
               </span>
             </h1>
@@ -311,23 +315,23 @@ const LandingPage = () => {
               <span className="font-semibold"> info@ka-tch.com
               </span>
             </p>
-            {!isFormSubmitted ? 
-            <div>
-              <label className="form-control w-full max-w-xs">
-                <input type="text" placeholder="Full Name" name="name" value={name} onChange={handleChangeInput} className="input input-bordered w-full max-w-xs" />
-                <input type="text" placeholder="Email" name="email" value={email} onChange={handleChangeInput} className="input input-bordered w-full max-w-xs mt-3" />
-                <textarea className="textarea textarea-bordered mt-3" placeholder="Message" name="message" value={message} onChange={handleChangeInput}></textarea>
-              </label>
-              <button className="btn w-full max-w-xs mt-3  bg-katech-red border-katech-red text-white hover:bg-red-600 hover:border-red-600" onClick={handleSubmit}>{loadingSubmit ? 'Sending': 'Send Message'}</button>
-            </div>
-            :
-            <div>
-              <h3>Thank you for contacting us! We will respond as soon as possible!</h3>
-              </div>}
+            {!isFormSubmitted ? (
+              <div>
+                <label className="form-control w-full max-w-xs">
+                  <input type="text" placeholder="Full Name" name="name" value={name} onChange={handleChangeInput} className="input input-bordered w-full max-w-xs" />
+                  <input type="text" placeholder="Email" name="email" value={email} onChange={handleChangeInput} className="input input-bordered w-full max-w-xs mt-3" />
+                  <textarea className="textarea textarea-bordered mt-3" placeholder="Message" name="message" value={message} onChange={handleChangeInput}></textarea>
+                </label>
+                <button className="btn w-full max-w-xs mt-3 bg-katech-red border-katech-red text-white hover:bg-red-600 hover:border-red-600" onClick={handleSubmit}>{loadingSubmit ? 'Sending' : 'Send Message'}</button>
+              </div>
+            ) : (
+              <div>
+                <h3>Thank you for contacting us! We will respond as soon as possible!</h3>
+              </div>
+            )}
           </div>
         </div>
       </div>
-
       <footer className="footer footer-center bg-base-300 text-base-content p-4">
         <aside>
           <p>Copyright © {new Date().getFullYear()} - All right reserved by Ka Technologies</p>
@@ -335,6 +339,6 @@ const LandingPage = () => {
       </footer>
     </div>
   );
-}
+};
 
 export default LandingPage;
