5fc6cdd042cc34d3edf279a675422a577346fdf2
mrkangs <gykang00@gmail.com>
2024-08-05 02:01:41 -0400

    connect backend with services and offerings

--- backend/schemaTypes/index.ts
+++ backend/schemaTypes/index.ts
@@ -1,9 +1,6 @@
 import people from './people';
 import contact from './contact';
 import service from './service';
-import service_overview from './service_overview';
-import project from './project';
-import project_overview from './project_overview';
+import offering from './offering';
 
-
-export const schemaTypes = [service, service_overview, people, contact]
+export const schemaTypes = [service, offering, people, contact]
